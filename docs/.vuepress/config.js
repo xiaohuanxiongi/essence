363205402f33ef2bbbd1f61214d9795ed0dd0821
香辣脆小浣熊 <xianglacuixiaohuanxiong@gmail.com>
2020-07-14 11:12:39 +0800

    init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,11 @@
 module.exports = {
   title: '计算机应用与科学',
-  description: '计算机应用与科学的知识要点'
+  description: '计算机应用与科学的知识要点',
+  base : '/essence/',
+  themeConfig : {
+    sidebarDepth: 1,
+    sidebar: [
+      ['../index/index.md', '计算机网络'],
+    ],
+  }
 }
