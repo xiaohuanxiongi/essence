691c97ee4ae7d70e70f71bccb08b6962e894b052
香辣脆小浣熊 <xianglacuixiaohuanxiong@gmail.com>
2021-11-05 16:02:51 +0800

    fix: 脚本修改

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env sh
 
+# 发生错误时终止
 set -e
-
-yarn docs:build
-
+# 构建
+yarn build
+# 到构建的输出目录下
 cd docs/.vuepress/dist
-
+#	创建git仓库
 git init
+# 缓存所有东西
 git add -A
-git commit -m 'deploy'
-
-git push -f https://xianglacuixiaohuanxiong@github.com/xianglacuixiaohuanxiong/essence.git master:gh-pages
-
+# 提交信息
+git commit -m "部署"
+# 提交到指定分支
+git push -f git@github.com:xiaohuanxiong0985/essence.git master:gh-pages
+# 返回到当前目录
 cd -
+# 删除包文件
+rm -rf dist
